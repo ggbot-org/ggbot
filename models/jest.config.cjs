ed85adf5e6da34b18eac4f0780d22206e1886f78
fibo <casati_gianluca@yahoo.it>
2022-07-23 14:10:04 +0200

    add moduleNameMapper config to support .js imports

--- models/jest.config.cjs
+++ models/jest.config.cjs
@@ -1,5 +1,8 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  moduleNameMapper: {
+    "(.+)\\.js": "$1",
+  },
   preset: "ts-jest",
   testEnvironment: "node",
 };
