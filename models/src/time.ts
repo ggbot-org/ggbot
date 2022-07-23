e91888f4c14bc6db3e47205825d9854c1e7554ee
fibo <casati_gianluca@yahoo.it>
2022-07-23 12:28:09 +0200

    add timestamp utils

--- models/src/time.ts
+++ models/src/time.ts
@@ -1,7 +1,28 @@
-import type { Timestamp } from "@ggbot2/time";
+import { Timestamp, now } from "@ggbot2/time";
+
+// Create
+// ///////////////////////////////////////////////////////////////////
 
 export type CreationTime = { readonly whenCreated: Timestamp };
 
+export type CreatedNow = () => CreationTime;
+
+export const createdNow: CreatedNow = () => ({ whenCreated: now() });
+
+// Delete
+// ///////////////////////////////////////////////////////////////////
+
 export type DeletionTime = { readonly whenDeleted: Timestamp };
 
+export type DeletedNow = () => DeletionTime;
+
+export const deletedNow: DeletedNow = () => ({ whenDeleted: now() });
+
+// Update
+// ///////////////////////////////////////////////////////////////////
+
 export type UpdateTime = { readonly whenUpdated: Timestamp };
+
+export type UpdatedNow = () => UpdateTime;
+
+export const updatedNow: UpdatedNow = () => ({ whenUpdated: now() });
