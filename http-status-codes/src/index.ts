3391e30cbef031f61333fafdc622e72b5ffe2c03
fibo <casati_gianluca@yahoo.it>
2022-07-23 12:13:03 +0200

    add http-status-codes workspace

---
+++ http-status-codes/src/index.ts
@@ -0,0 +1,26 @@
+// See also https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+
+// 2XX success
+// ///////////////////////////////////////////////////////////////////
+
+// Standard response for successful HTTP requests.
+export const __200__OK__ = 200;
+
+// 4XX client errors
+// ///////////////////////////////////////////////////////////////////
+
+// The server cannot or will not process the request due to an apparent client error
+// (e.g., malformed request syntax)
+export const __400__BAD_REQUEST__ = 400;
+
+// A request method is not supported for the requested resource;
+// for example, a GET request on a form that requires data to be presented via POST,
+// or a PUT request on a read-only resource.
+export const __405__METHOD_NOT_ALLOWED__ = 405;
+
+// 5XX server errors
+// ///////////////////////////////////////////////////////////////////
+
+// A generic error message, given when an unexpected condition was encountered
+// and no more specific message is suitable.
+export const __500__INTERNAL_SERVER_ERROR__ = 500;
