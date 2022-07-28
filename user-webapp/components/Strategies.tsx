5d3181fd5e33ac1ca9cb23c72a973efdb2bdec27
fibo <casati_gianluca@yahoo.it>
2022-07-28 19:10:34 +0200

    persist selectedStrategyKey in sessionStorage

--- user-webapp/components/Strategies.tsx
+++ user-webapp/components/Strategies.tsx
@@ -1,4 +1,4 @@
-import { StrategyKey } from "@ggbot2/models";
+import { StrategyKey, isStrategyKey } from "@ggbot2/models";
 import { Button } from "@ggbot2/ui-components";
 import { useRouter } from "next/router";
 import { FC, SyntheticEvent, useCallback, useMemo, useState } from "react";
@@ -6,12 +6,34 @@ import { SchedulingStatusBadge, StrategyActions } from "_components";
 import { useApiAction } from "_hooks";
 import { route } from "_routing";
 
+type SelectedStrategyKey = StrategyKey | null;
+const selectedStrategyKeyStorageKey = "selectedStrategyKey";
+
+const getStoredSelectedStrategy = (): SelectedStrategyKey => {
+  const storedValue = global?.sessionStorage?.getItem(
+    selectedStrategyKeyStorageKey
+  );
+  if (!storedValue) return null;
+  const objValue = JSON.parse(storedValue);
+  return isStrategyKey(objValue) ? objValue : null;
+};
+
+const setStoredSelectedStrategyKey = (
+  selectedStrategyKey: SelectedStrategyKey
+) => {
+  if (selectedStrategyKey)
+    global?.sessionStorage?.setItem(
+      selectedStrategyKeyStorageKey,
+      JSON.stringify(selectedStrategyKey)
+    );
+  else global?.sessionStorage?.removeItem(selectedStrategyKeyStorageKey);
+};
+
 export const Strategies: FC = () => {
   const router = useRouter();
 
-  const [selectedStrategyKey, setSelectedStrategyKey] = useState<
-    StrategyKey | undefined
-  >();
+  const [selectedStrategyKey, setSelectedStrategyKey] =
+    useState<SelectedStrategyKey>(getStoredSelectedStrategy());
 
   const { data: strategies } = useApiAction.READ_ACCOUNT_STRATEGY_LIST();
 
@@ -32,6 +54,7 @@ export const Strategies: FC = () => {
             name,
             onClick: (event: SyntheticEvent) => {
               event.stopPropagation();
+              setStoredSelectedStrategyKey({ strategyId, strategyKind });
               setSelectedStrategyKey({ strategyId, strategyKind });
             },
             schedulingStatus,
@@ -43,13 +66,13 @@ export const Strategies: FC = () => {
     [selectedStrategyKey, setSelectedStrategyKey, strategies]
   );
 
+  const onClickContainer = useCallback(() => {
+    setStoredSelectedStrategyKey(null);
+    setSelectedStrategyKey(null);
+  }, [setSelectedStrategyKey]);
+
   return (
-    <div
-      className="p-4 flex flex-col gap-4"
-      onClick={() => {
-        setSelectedStrategyKey(undefined);
-      }}
-    >
+    <div className="p-4 flex flex-col gap-4" onClick={onClickContainer}>
       <span className="text-xl">strategies</span>
       <menu>
         <Button onClick={onClickNewStrategy}>new strategy</Button>
@@ -65,7 +88,7 @@ export const Strategies: FC = () => {
             strategyKind,
           }) => (
             <div
-              className="flex flex-col gap-4 shadow p-2 hover:shadow-md transition-all duration-200"
+              className="flex flex-col gap-4 shadow p-2 hover:shadow-md transition-all"
               key={strategyId}
             >
               <div
