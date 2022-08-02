dc17eef9ea210921f1e5da7f1630d6eeb9e3db42
fibo <casati_gianluca@yahoo.it>
2022-08-02 10:52:40 +0200

    activate tailwind intellisense

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+// This file is not used by workspaces but it is here to activate
+// editor plugins or other tools that expect a tailwind config file in the
+// project root folder.
+const theme = require("./ui-components/tailwind.theme");
+
+module.exports = {
+  // List here every folder in any workspace that uses tailwind.
+  content: [
+    "./admin-webapp/components/**/*.{ts,tsx}",
+    "./admin-webapp/pages/**/*.{ts,tsx}",
+    "./ui-components/examples/**/*.{ts,tsx}",
+    "./ui-components/pages/**/*.{ts,tsx}",
+    "./ui-components/src/**/*.{ts,tsx}",
+    "./user-webapp/components/**/*.{ts,tsx}",
+    "./user-webapp/pages/**/*.{ts,tsx}",
+  ],
+  plugins: [],
+  theme,
+};
