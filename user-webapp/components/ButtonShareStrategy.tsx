3d98ce00ed5c2aa299e13b48adb9e8c4e0388d96
fibo <casati_gianluca@yahoo.it>
2022-09-20 22:25:48 +0200

    try to use WebShare API

--- user-webapp/components/ButtonShareStrategy.tsx
+++ user-webapp/components/ButtonShareStrategy.tsx
@@ -1,17 +1,45 @@
 import { StrategyKey } from "@ggbot2/models";
 import { Button } from "@ggbot2/ui-components";
-import { FC, useCallback } from "react";
+import { FC, useCallback, useMemo } from "react";
 import { toast } from "react-hot-toast";
 import { route, webappBaseUrl } from "_routing";
 
 type Props = StrategyKey;
 
+// TODO try it on iPhone
 export const ButtonShareStrategy: FC<Props> = (strategyKey) => {
-  const onClick = useCallback(() => {
-    const sharableLink = `${webappBaseUrl}${route.viewFlowPage(strategyKey)}`;
-    navigator.clipboard.writeText(sharableLink);
-    toast("Link copied");
-  }, [strategyKey]);
+  const _navigator = global?.window?.navigator;
+
+  const canShare = useMemo(
+    () =>
+      _navigator &&
+      ("clipboard" in _navigator ||
+        ("share" in _navigator && "canShare" in _navigator)),
+    [_navigator]
+  );
+
+  const url = useMemo(
+    () => `${webappBaseUrl}${route.viewFlowPage(strategyKey)}`,
+    [strategyKey]
+  );
+
+  const copyToClipboard = useCallback(() => {
+    try {
+      _navigator?.clipboard.writeText(url);
+      toast("Strategy link copied");
+    } catch (_ignore) {}
+  }, [_navigator, url]);
+
+  const onClick = useCallback(async () => {
+    try {
+      if (_navigator.canShare({ url })) _navigator.share({ url });
+      else copyToClipboard();
+    } catch (_ignore) {
+      copyToClipboard();
+    }
+  }, [_navigator, copyToClipboard, url]);
+
+  if (!canShare) return null;
 
   return <Button onClick={onClick}>share</Button>;
 };
