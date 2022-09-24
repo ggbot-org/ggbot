57a61c3e639384cf8d516a37265768794488a2a7
fibo <casati_gianluca@yahoo.it>
2022-09-24 16:34:31 +0200

    allowJS

--- devops/src/_isMainModule.js
+++ devops/src/_isMainModule.js
@@ -1,12 +1,8 @@
 import { fileURLToPath } from "node:url";
 
 /**
- * @param {String} importMetaUrl
+ * @param {string} importMetaUrl
  */
-export const isMainModule = (importMetaUrl) => {
-  if (importMetaUrl.startsWith("file:")) {
-    const modulePath = fileURLToPath(importMetaUrl);
-    return process.argv[1] === modulePath;
-  }
-  return false;
-};
+export const isMainModule = (importMetaUrl) =>
+  importMetaUrl.startsWith("file:") &&
+  fileURLToPath(importMetaUrl) === process.argv[1];
