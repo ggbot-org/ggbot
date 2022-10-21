c947764e287dbe6ffa2d15ed1d2ae71c6c017ae3
fibo <casati_gianluca@yahoo.it>
2022-10-21 21:00:37 +0200

    update BucketCannedACL

--- infrastructure/src/s3.ts
+++ infrastructure/src/s3.ts
@@ -13,7 +13,7 @@ export const getAssetsBucketName = () => assetsDomain;
 
 export const getAssetsBucketArn = () => `arn:aws:s3:::${getAssetsBucketName()}`;
 
-export const assetsBucketACL: BucketCannedACL = "public-read";
+export const assetsBucketACL = BucketCannedACL.public_read;
 
 export const getDataBucketName = (deployStage = defaultDeployStage) =>
   `${deployStage}-data.${domainName}`;
@@ -21,7 +21,7 @@ export const getDataBucketName = (deployStage = defaultDeployStage) =>
 export const getDataBucketArn = (deployStage = defaultDeployStage) =>
   `arn:aws:s3:::${getDataBucketName(deployStage)}`;
 
-export const dataBucketACL: BucketCannedACL = "private";
+export const dataBucketACL = BucketCannedACL.private;
 
 export const getLogsBucketName = (deployStage = defaultDeployStage) =>
   `${deployStage}-logs.${domainName}`;
@@ -29,23 +29,23 @@ export const getLogsBucketName = (deployStage = defaultDeployStage) =>
 export const getLogsBucketArn = (deployStage = defaultDeployStage) =>
   `arn:aws:s3:::${getLogsBucketName(deployStage)}`;
 
-export const logsBucketACL: BucketCannedACL = "private";
+export const logsBucketACL = BucketCannedACL.private;
 
 export const getNakedDomainBucketName = () => domainName;
 
 export const getNakedDomainBucketArn = () =>
   `arn:aws:s3:::${getNakedDomainBucketName()}`;
 
-export const nakedDomainBucketACL: BucketCannedACL = "public-read";
+export const nakedDomainBucketACL = BucketCannedACL.public_read;
 
 export const getDesignBucketName = () => designDomain;
 
 export const getDesignBucketArn = () => `arn:aws:s3:::${getDesignBucketName()}`;
 
-export const designBucketACL: BucketCannedACL = "public-read";
+export const designBucketACL = BucketCannedACL.public_read;
 
 export const getWwwBucketName = () => wwwDomain;
 
 export const getWwwBucketArn = () => `arn:aws:s3:::${getWwwBucketName()}`;
 
-export const wwwBucketACL: BucketCannedACL = "public-read";
+export const wwwBucketACL = BucketCannedACL.public_read;
