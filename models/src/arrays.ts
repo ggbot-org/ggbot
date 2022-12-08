5ed533bf7f64fcd59f8938707420103365f0a1b4
fibo <fibo@users.noreply.github.com>
2022-12-08 21:01:57 +0100

    add doc

--- models/src/arrays.ts
+++ models/src/arrays.ts
@@ -1,3 +1,11 @@
+/** Use `arrayTypeGuard` as a *type guard* helper to reduce boilerplate.
+
+@example
+```ts
+export type MyItems = MyItem[];
+
+const isMyItems = arrayTypeGuard<MyItem>(isMyItem);
+``` */
 export const arrayTypeGuard =
   <T>(check: (item: unknown) => item is T) =>
   (arg: unknown): arg is T[] =>
