b50fa18ed555f24999f413e769bb23d0a3fd0989
fibo <fibo@users.noreply.github.com>
2023-01-31 09:27:48 +0100

    rename to uppercase Executor

---
+++ executor/src/Executor_test.ts
@@ -0,0 +1,19 @@
+import assert from "node:assert/strict";
+import { describe, it } from "node:test";
+// import { Executor } from "./Executor.js";
+
+describe("test", () => {
+  it("works", () => {
+    assert.ok(true);
+  });
+});
+// TODO
+// describe("executor.managesItem()", () => {
+//   it("works", () => {
+//     const capacity = 2;
+//     const index = 1;
+//     const itemId = "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa";
+//     const executor = new Executor(capacity, index);
+//     assert.ok(executor.managesItem(itemId));
+//   });
+// });
