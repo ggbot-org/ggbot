b565cd70540d19cb523d608919b60f6167cc6d47
fibo <fibo@users.noreply.github.com>
2023-04-02 13:30:14 +0200

    remove isomorphic-fetch

--- binance/src/connector.ts
+++ binance/src/connector.ts
@@ -1,9 +1,12 @@
 import { ErrorHTTP } from "@ggbot2/http-status-codes";
-import fetch from "isomorphic-fetch";
 import { BinanceApiEndpoint } from "./endpoints.js";
 
 const defaultBaseUrl = "https://api.binance.com";
-const apiClusters = ["https://api1.binance.com", "https://api2.binance.com", "https://api3.binance.com"];
+const apiClusters = [
+  "https://api1.binance.com",
+  "https://api2.binance.com",
+  "https://api3.binance.com",
+];
 const baseUrls = [defaultBaseUrl, ...apiClusters] as const;
 type BaseUrl = typeof baseUrls[number];
 
@@ -13,11 +16,18 @@ export class BinanceConnector {
   static defaultBaseUrl = defaultBaseUrl;
   readonly baseUrl: BaseUrl;
 
-  constructor({ baseUrl = BinanceConnector.defaultBaseUrl }: BinanceConnectorConstructorArg) {
+  constructor({
+    baseUrl = BinanceConnector.defaultBaseUrl,
+  }: BinanceConnectorConstructorArg) {
     this.baseUrl = baseUrl;
   }
 
-  async request<Data>({ apiKey = "", endpoint, method, params = {} }: BinanceConnectorRequestArg) {
+  async request<Data>({
+    apiKey = "",
+    endpoint,
+    method,
+    params = {},
+  }: BinanceConnectorRequestArg) {
     const url = new URL(this.baseUrl);
     url.pathname = endpoint;
 
@@ -25,21 +35,18 @@ export class BinanceConnector {
       if (value === undefined) continue;
       // The array conversion logic is different from usual query string.
       // E.g. symbols=["BTCUSDT","BNBBTC"] instead of symbols[]=BTCUSDT&symbols[]=BNBBTC
-      const valueString = Array.isArray(value) ? `["${value.join('","')}"]` : value;
+      const valueString = Array.isArray(value)
+        ? `["${value.join('","')}"]`
+        : value;
       url.searchParams.append(key, String(valueString));
     }
 
-    // TODO when updating to Node 18 use
-    // const headers = new Headers({
-    const headers = {
+    const headers = new Headers({
       ...(apiKey ? { "X-MBX-APIKEY": apiKey } : {}),
-    };
+    });
 
     const fetchOptions: RequestInit = { headers, method };
 
-    // TODO when updating to Node 18 use native fetch
-    // by now Amazon Linux support Node 16
-    // @ts-ignore
     const response = await fetch(url, fetchOptions);
     if (!response.ok) throw new ErrorHTTP(response);
 
