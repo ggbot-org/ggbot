de8b65ded3b90bb84207106ed3e85d14e5ad8cdd
Gianluca Casati <fibo@users.noreply.github.com>
2023-09-13 15:01:38 +0200

    fix Safari favicon

--- webapp/src/html/content.ts
+++ webapp/src/html/content.ts
@@ -26,9 +26,13 @@ type HeadTagArgs = {
 
 type HtmlTagArgs = HeadTagArgs & BodyTagArgs
 
+// Use default path for favicon.ico, i.e. put in the root public folder,
+// in case some browser does not read the `link rel="icon"` tag.
+const faviconIcoUrl = "/favicon.ico"
+
 const logoBaseUrl = "/logo"
-const faviconIcoUrl = `${logoBaseUrl}/favicon.ico`
 const faviconSvgUrl = `${logoBaseUrl}/favicon.svg`
+const appleTouchIconUrl = `${logoBaseUrl}/logo-180.png`
 
 const fontBaseUrl = "https://rsms.me"
 const fontFaceUrl = `${fontBaseUrl}/inter/inter.css`
@@ -47,8 +51,9 @@ const styleTag = ({ content }: StyleTag) => `<style>${content}</style>`
 const baseStyle = () =>
 	[`html, body, #${reactRootId} { min-height: 100vh; }`].join("\n")
 
-const faviconTags = [
+const logoTags = [
 	`<link rel="icon" href="${faviconIcoUrl}" sizes="any">`,
+	`<link rel="apple-touch-icon" sizes="180x180" href="${appleTouchIconUrl}">`,
 	`<link rel="icon" href="${faviconSvgUrl}" type="image/svg+xml">`
 ]
 
@@ -61,7 +66,7 @@ const metaTags = ({ title }: HeadTagArgs["meta"]) => [
 	'<meta charset="UTF-8" />',
 	'<meta name="viewport" content="width=device-width" />',
 	`<title>${title}</title>`,
-	...faviconTags,
+	...logoTags,
 	...fontTags
 ]
 
