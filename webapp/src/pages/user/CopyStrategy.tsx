7a5a66056e0d503d6b368b84b7cb7cbba2c74b79
Gianluca Casati <fibo@users.noreply.github.com>
2023-10-03 11:48:56 +0200

    fix copy

--- webapp/src/pages/user/CopyStrategy.tsx
+++ webapp/src/pages/user/CopyStrategy.tsx
@@ -1,5 +1,6 @@
 import { CopyStrategy } from "_/components/user/CopyStrategy"
 import { PageContainer } from "_/components/user/PageContainer"
+import { StrategyProvider } from "_/contexts/Strategy"
 import { ManageStrategyProvider } from "_/contexts/user/ManageStrategy"
 import { StrategiesProvider } from "_/contexts/user/Strategies"
 import { FC } from "react"
@@ -7,9 +8,11 @@ import { FC } from "react"
 export const CopyStrategyPage: FC = () => (
 	<PageContainer>
 		<StrategiesProvider>
-			<ManageStrategyProvider>
-				<CopyStrategy />
-			</ManageStrategyProvider>
+			<StrategyProvider>
+				<ManageStrategyProvider>
+					<CopyStrategy />
+				</ManageStrategyProvider>
+			</StrategyProvider>
 		</StrategiesProvider>
 	</PageContainer>
 )
