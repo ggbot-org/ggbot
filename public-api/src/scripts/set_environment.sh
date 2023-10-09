ef2393392d1f853d9a6fd3fa395c96c5072d9add
Gianluca Casati <fibo@users.noreply.github.com>
2023-10-09 19:54:48 +0200

    fix environment variables syntax

--- public-api/src/scripts/set_environment.sh
+++ public-api/src/scripts/set_environment.sh
@@ -3,4 +3,4 @@
 DIR=$(dirname "$0")
 source $DIR/_common_variables.sh
 
-aws lambda update-function-configuration --region $AWS_REGION --function-name $ACTION_FUNCTION_NAME --environment "Variables={AWS_ACCOUNT_ID=${AWS_ACCOUNT_ID},AWS_DATA_REGION=${AWS_DATA_REGION},DNS_DOMAIN=${DNS_DOMAIN},DEPLOY_STAGE=$DEPLOY_STAGE}"
+aws lambda update-function-configuration --region $AWS_REGION --function-name $ACTION_FUNCTION_NAME --environment "Variables={AWS_ACCOUNT_ID=$AWS_ACCOUNT_ID,AWS_DATA_REGION=$AWS_DATA_REGION,DNS_DOMAIN=$DNS_DOMAIN,DEPLOY_STAGE=$DEPLOY_STAGE}"
