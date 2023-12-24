a5db20cdfa4e068b0a7bbc9e401a481f1bc9504c
Gianluca Casati <fibo@users.noreply.github.com>
2023-12-24 10:48:22 +0700

    improve globalThis namespace declaration

--- webapp/src/types/Window.d.ts
+++
@@ -1,7 +0,0 @@
-import { LoggingController } from "_/logging"
-
-declare global {
-	interface Window {
-		log: LoggingController
-	}
-}
--- webapp/src/types/global.d.ts
+++ webapp/src/types/global.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-var */
 import { esbuildDefinitions } from "../esbuild/definitions"
 import { LoggingController } from "../logging"
 
@@ -6,8 +7,8 @@ declare global {
 	const DEPLOY_STAGE: typeof esbuildDefinitions.DEPLOY_STAGE
 	const DNS_DOMAIN: typeof esbuildDefinitions.DNS_DOMAIN
 	const IS_DEV: typeof esbuildDefinitions.IS_DEV
-	// To be able to use `globalThis.log`, it is needed to declare it as `var`
-	// See: https://stackoverflow.com/a/69230938/1217468
-	// eslint-disable-next-line no-var
-	var log: LoggingController
+
+	namespace globalThis {
+		var log: LoggingController
+	}
 }
