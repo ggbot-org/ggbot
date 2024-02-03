d42b141089edf8a94c5a53ac29f012a5b711a22a
fibo <fibo@users.noreply.github.com>
2024-02-03 22:23:05 +0100

    fix copy button

--- webapp/src/components/GoCopyStrategy.tsx
+++ webapp/src/components/GoCopyStrategy.tsx
@@ -1,24 +1,24 @@
 import { Button } from "_/components/library"
 import { StrategyContext } from "_/contexts/Strategy"
 import { webapp } from "_/routing/webapp"
-import { FC, useContext } from "react"
+import { FC, useCallback, useContext } from "react"
 import { FormattedMessage } from "react-intl"
 
 export const GoCopyStrategy: FC = () => {
 	const { strategyKey } = useContext(StrategyContext)
 
+	const onClick = useCallback(() => {
+		if (!strategyKey) return
+		const url = webapp.user.copyStrategy(strategyKey)
+		// @ts-expect-error
+		location = `${url.pathname}${url.search}`
+	}, [strategyKey])
+
 	if (!strategyKey) return null
 
-	const strategyUrl = webapp.user.copyStrategy(strategyKey)
-
 	return (
-		<form
-			method="GET"
-			action={`${strategyUrl.pathname}${strategyUrl.search}`}
-		>
-			<Button>
-				<FormattedMessage id="GoCopyStrategy.label" />
-			</Button>
-		</form>
+		<Button onClick={onClick}>
+			<FormattedMessage id="GoCopyStrategy.label" />
+		</Button>
 	)
 }
