c7eda96a60522b9a6c0f46040a9ffbc549d7f2f4
fibo <fibo@users.noreply.github.com>
2024-03-22 16:20:46 +0100

    remove end-to-end-tests workspace

--- end-to-end-tests/.eslintrc.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	extends: ["@workspace/eslint-config", "plugin:playwright/recommended"],
-	parserOptions: {
-		project: true,
-		tsconfigRootDir: __dirname
-	}
-}
--- end-to-end-tests/src/webapp.ts
+++
@@ -1,4 +0,0 @@
-import { ENV } from "@workspace/env"
-import { WebappURLs } from "@workspace/locators"
-
-export const webapp = new WebappURLs("next", ENV.DNS_DOMAIN())
