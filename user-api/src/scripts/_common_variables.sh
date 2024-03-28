eee4a86b115f4f2348619170b910847ddefafae1
fibo <fibo@users.noreply.github.com>
2024-03-28 16:21:21 +0100

    disable aws CLI pagination

--- user-api/src/scripts/_common_variables.sh
+++ user-api/src/scripts/_common_variables.sh
@@ -5,6 +5,9 @@ RUNTIME="nodejs18.x"
 # TODO create a ggbot2-main-api-role ggbot2-next-api-role ggbot2-local-api-role (copy of next)
 ROLE="arn:aws:iam::$AWS_ACCOUNT_ID:role/ggbot2_api_role"
 
+# Disable pagination.
+export AWS_PAGER=""
+
 ACTION_FUNCTION_NAME="ggbot2-${DEPLOY_STAGE}-user-api-action"
 ACTION_FUNCTION_LOG_GROUP_NAME="/aws/lambda/$ACTION_FUNCTION_NAME"
 ACTION_FUNCTION_ZIP_FILE="fileb://dist/action.zip"
