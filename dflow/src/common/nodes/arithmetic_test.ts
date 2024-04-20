976f5600cc149c5526416b5c5b9076028acc2d79
Gianluca Casati <fibo@users.noreply.github.com>
2024-04-20 09:53:48 +0200

    enhance arithmetic nodes tests

--- dflow/src/common/nodes/arithmetic_test.ts
+++ dflow/src/common/nodes/arithmetic_test.ts
@@ -1,163 +1,134 @@
 import { strict as assert } from "node:assert"
-import { describe, test } from "node:test"
+import { test } from "node:test"
 
+import { Dflow } from "dflow"
 import { now } from "minimal-time-helpers"
 
 import { getDflowExecutionOutputData } from "../executor.js"
 import { DflowExecutorMock } from "../mocks/executor.js"
+import {
+	Addition,
+	Division,
+	Multiplication,
+	Subtraction
+} from "./arithmetic.js"
 
-void describe("add", () => {
-	void test("implements addition", async () => {
-		const nodeId = "operator"
-		const a = 2
-		const b = 3
-		const executor = new DflowExecutorMock({
-			view: {
-				nodes: [
-					{
-						id: "i1",
-						text: JSON.stringify(a),
-						outs: [{ id: "o1" }]
-					},
-					{
-						id: "i2",
-						text: JSON.stringify(b),
-						outs: [{ id: "o2" }]
-					},
-					{
-						id: nodeId,
-						text: "add",
-						ins: [{ id: "a" }, { id: "b" }]
-					}
-				],
-				edges: [
-					{ id: "e1", from: ["i1", "o1"], to: [nodeId, "a"] },
-					{ id: "e2", from: ["i2", "o2"], to: [nodeId, "b"] }
-				]
-			}
-		})
-		const { execution } = await executor.run({
-			params: {},
-			memory: {},
-			time: now()
-		})
-		assert.equal(getDflowExecutionOutputData(execution, nodeId, 0), 5)
-	})
+void test("add", async () => {
+	const operator: ArithmeticNodeKind = "add"
+	const testData: ExecuteOperatorTestData[] = [
+		{
+			input: { a: 2, b: 3 },
+			output: 5
+		}
+	]
+
+	for (const { input, output } of testData) {
+		const result = await executeOperator(operator, input)
+		assert.equal(result, output)
+	}
 })
 
-void describe("sub", () => {
-	void test("implements subtraction", async () => {
-		const nodeId = "operator"
-		const a = 2
-		const b = 3
-		const executor = new DflowExecutorMock({
-			view: {
-				nodes: [
-					{
-						id: "i1",
-						text: JSON.stringify(a),
-						outs: [{ id: "o1" }]
-					},
-					{
-						id: "i2",
-						text: JSON.stringify(b),
-						outs: [{ id: "o2" }]
-					},
-					{
-						id: nodeId,
-						text: "sub",
-						ins: [{ id: "a" }, { id: "b" }]
-					}
-				],
-				edges: [
-					{ id: "e1", from: ["i1", "o1"], to: [nodeId, "a"] },
-					{ id: "e2", from: ["i2", "o2"], to: [nodeId, "b"] }
-				]
-			}
-		})
-		const { execution } = await executor.run({
-			params: {},
-			memory: {},
-			time: now()
-		})
-		assert.equal(getDflowExecutionOutputData(execution, nodeId, 0), a - b)
-	})
+void test("sub", async () => {
+	const operator: ArithmeticNodeKind = "sub"
+	const testData: ExecuteOperatorTestData[] = [
+		{
+			input: { a: 2, b: 3 },
+			output: -1
+		}
+	]
+
+	for (const { input, output } of testData) {
+		const result = await executeOperator(operator, input)
+		assert.equal(result, output)
+	}
 })
 
-void describe("mul", () => {
-	void test("implements multiplication", async () => {
-		const nodeId = "operator"
-		const a = 2
-		const b = 3
-		const executor = new DflowExecutorMock({
-			view: {
-				nodes: [
-					{
-						id: "i1",
-						text: JSON.stringify(a),
-						outs: [{ id: "o1" }]
-					},
-					{
-						id: "i2",
-						text: JSON.stringify(b),
-						outs: [{ id: "o2" }]
-					},
-					{
-						id: nodeId,
-						text: "mul",
-						ins: [{ id: "a" }, { id: "b" }]
-					}
-				],
-				edges: [
-					{ id: "e1", from: ["i1", "o1"], to: [nodeId, "a"] },
-					{ id: "e2", from: ["i2", "o2"], to: [nodeId, "b"] }
-				]
-			}
-		})
-		const { execution } = await executor.run({
-			params: {},
-			memory: {},
-			time: now()
-		})
-		assert.equal(getDflowExecutionOutputData(execution, nodeId, 0), a * b)
-	})
+void test("mul", async () => {
+	const operator: ArithmeticNodeKind = "mul"
+	const testData: ExecuteOperatorTestData[] = [
+		{
+			input: { a: 2, b: 3 },
+			output: 6
+		}
+	]
+
+	for (const { input, output } of testData) {
+		const result = await executeOperator(operator, input)
+		assert.equal(result, output)
+	}
 })
 
-void describe("div", () => {
-	void test("implements multiplication", async () => {
-		const nodeId = "operator"
-		const a = 3
-		const b = 2
-		const executor = new DflowExecutorMock({
-			view: {
-				nodes: [
-					{
-						id: "i1",
-						text: JSON.stringify(a),
-						outs: [{ id: "o1" }]
-					},
-					{
-						id: "i2",
-						text: JSON.stringify(b),
-						outs: [{ id: "o2" }]
-					},
-					{
-						id: nodeId,
-						text: "div",
-						ins: [{ id: "a" }, { id: "b" }]
-					}
-				],
-				edges: [
-					{ id: "e1", from: ["i1", "o1"], to: [nodeId, "a"] },
-					{ id: "e2", from: ["i2", "o2"], to: [nodeId, "b"] }
-				]
-			}
-		})
-		const { execution } = await executor.run({
-			params: {},
-			memory: {},
-			time: now()
-		})
-		assert.equal(getDflowExecutionOutputData(execution, nodeId, 0), a / b)
-	})
+void test("div", async () => {
+	const operator: ArithmeticNodeKind = "div"
+	const testData: ExecuteOperatorTestData[] = [
+		{
+			input: { a: 3, b: 2 },
+			output: 1.5
+		}
+	]
+
+	for (const { input, output } of testData) {
+		const result = await executeOperator(operator, input)
+		assert.equal(result, output)
+	}
 })
+
+const arithmeticOperators = [
+	Addition.kind,
+	Subtraction.kind,
+	Multiplication.kind,
+	Division.kind
+] as const
+type ArithmeticNodeKind = (typeof arithmeticOperators)[number]
+
+type ExecuteOperatorInput = {
+	a: number
+	b: number
+}
+
+type ExecuteOperatorOutput = number | undefined
+
+type ExecuteOperatorTestData = {
+	input: ExecuteOperatorInput
+	output: ExecuteOperatorOutput
+}
+
+const executeOperator = async (
+	nodeKind: ArithmeticNodeKind,
+	{ a, b }: ExecuteOperatorInput
+): Promise<ExecuteOperatorOutput> => {
+	const nodeId = "operator"
+	const executor = new DflowExecutorMock({
+		view: {
+			nodes: [
+				{
+					id: "i1",
+					text: JSON.stringify(a),
+					outs: [{ id: "o1" }]
+				},
+				{
+					id: "i2",
+					text: JSON.stringify(b),
+					outs: [{ id: "o2" }]
+				},
+				{
+					id: nodeId,
+					text: nodeKind,
+					ins: [{ id: "a" }, { id: "b" }]
+				}
+			],
+			edges: [
+				{ id: "e1", from: ["i1", "o1"], to: [nodeId, "a"] },
+				{ id: "e2", from: ["i2", "o2"], to: [nodeId, "b"] }
+			]
+		}
+	})
+	const { execution } = await executor.run({
+		params: {},
+		memory: {},
+		time: now()
+	})
+	const result = getDflowExecutionOutputData(execution, nodeId, 0)
+	if (result === undefined || Dflow.isNumber(result)) return result
+	throw new TypeError()
+}
