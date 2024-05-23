0412df736adfc0cccd43b9dd4ba89ddd8cb32d1d
fibo <fibo@users.noreply.github.com>
2024-05-23 17:13:20 +0200

    remove FormatjsIntlMessageIds.ts

--- webapp/src/scripts/generate_formatjs_types.ts
+++ webapp/src/scripts/generate_formatjs_types.ts
@@ -1,15 +1,13 @@
 import { join } from "node:path"
 
-import {
-	FormatjsIntlMessageIdsContent,
-	FormatjsIntlMessageIdsFilename
-} from "_/i18n/FormatjsIntlMessageIds.js"
 import { translationPathname } from "_/i18n/locales.js"
 import { defaultLanguage } from "@workspace/models"
 import readFile from "read-file-utf8"
 import writeFile from "write-file-utf8"
 
 import { typesDir, workspaceDir } from "../package.js"
+
+const FormatjsIntlMessageIdsFilename = "FormatjsIntlMessageIds.d.ts"
 
 const defaultTranslation = await readFile(
 	join(workspaceDir, translationPathname(defaultLanguage))
@@ -19,6 +17,19 @@ const pathname = join(typesDir, FormatjsIntlMessageIdsFilename)
 
 const translationKeys = Object.keys(defaultTranslation)
 
-const content = FormatjsIntlMessageIdsContent(translationKeys)
+const content = `// This file is generated
+
+// ts-prune-ignore-next
+export declare type FormatjsIntlMessageId =
+  | ${translationKeys.map((key) => `"${key}"`).join("\n  | ")};
+
+declare global {
+  namespace FormatjsIntl {
+    interface Message {
+      ids: FormatjsIntlMessageId;
+    }
+  }
+}
+`
 
 await writeFile(pathname, content)
