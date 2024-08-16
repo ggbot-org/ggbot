684ac46c85586705fda5e3397f43054a8692dc85
fibo <fibo@users.noreply.github.com>
2024-08-16 16:27:39 +0200

    try React strict mode

--- webapp/src/react/mount.tsx
+++ webapp/src/react/mount.tsx
@@ -1,14 +1,13 @@
-import { ReactNode } from "react"
+import { ReactNode, StrictMode } from "react"
 // eslint-disable-next-line import/no-internal-modules
 import { createRoot } from "react-dom/client"
 
 import { reactRootId } from "./root"
 
-export function mount(Page: ReactNode) {
+export function mount(page: ReactNode) {
 	createRoot(document.getElementById(reactRootId) as HTMLElement).render(
-		// Avoid using <StrictMode>
-		// it will trigger all `useEffect` twice, so all network requests will run twice.
-		// it is better to develop with same conditions the "user" has in production.
-		Page
+		<StrictMode>
+			{page}
+		</StrictMode>
 	)
 }
